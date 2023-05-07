2fbf4f195b6120f94da5975a8e6d9cd07d61b48d
Ashif <ashiful134@gmail.com>
2023-05-07 10:59:20 +0600

    logout

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,6 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/login', Auth\LoginController::class);
+Route::post('auth/logout', Auth\LogoutController::class);
+
 
